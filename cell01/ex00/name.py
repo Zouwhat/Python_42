a63d1a0ec2604555e8db75ff7ddfa038d044474f
Zouwhat <146157639+Zouwhat@users.noreply.github.com>
2025-09-15 12:40:20 +0700

    Update name.py

--- cell01/ex00/name.py
+++ cell01/ex00/name.py
@@ -1,8 +1,8 @@
 """ name.py """
 def main():
     """ name.py """
-    first_name = 'Watin'
-    last_name = 'Promfiy'
+    first_name = 'Guagoon'
+    last_name = 'Wasoontara'
     print(f'{first_name} {last_name}')
 
 main()
