da94fe1f7f21e5810316ca327934e2a67e956a48
Zouwhat <snipersoonza@hotmail.com>
2025-09-15 22:18:18 +0700

    update 4.1

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -6,7 +6,8 @@ def main():
         age = int(input('Please tell me your age: '))
         print(f'You are currently {age} years old')
         for i in range(1, 4):
-            print(f'In 10 years, you\'ll be {age+i*10} years old.')
+            before_age = i*10
+            print(f'In {before_age} years, you\'ll be {age+i*10} years old.')
     except:
         print('Mai Aow STR')
 
